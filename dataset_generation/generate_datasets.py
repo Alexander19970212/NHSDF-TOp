3d5edb694892d23a07eb4ff772f3a16f8020f5e1
Alexander19970212 <kalexu97@mail.ru>
2025-01-19 12:19:08 +0300

    [UPDATE] rec experiments

--- dataset_generation/generate_datasets.py
+++ dataset_generation/generate_datasets.py
@@ -20,57 +20,57 @@ def main(args):
 
     #################### train dataset ####################
 
-    dataset_path = f'{root_path}/ellipse_sdf_dataset_smf22_arc_ratio_5000.csv'
-    # dataset_path = '../../mnt/local/data/kalexu97/topOpt/ellipse_sdf_dataset_smf22_arc_ratio.csv'
-    df = generate_ellipse_sdf_dataset(num_ellipse=5000, points_per_ellipse=1000, smooth_factor=22, filename=dataset_path)
+    # dataset_path = f'{root_path}/ellipse_sdf_dataset_smf22_arc_ratio_5000.csv'
+    # # dataset_path = '../../mnt/local/data/kalexu97/topOpt/ellipse_sdf_dataset_smf22_arc_ratio.csv'
+    # df = generate_ellipse_sdf_dataset(num_ellipse=5000, points_per_ellipse=1000, smooth_factor=22, filename=dataset_path)
 
-    dataset_path = f'{root_path}/triangle_sdf_dataset_smf20_arc_ratio_5000.csv'
-    # dataset_path = '../../mnt/local/data/kalexu97/topOpt/rounded_triangle_sdf_dataset_smf20_arc_ratio.csv'
-    df = generate_rounded_triangle_sdf_dataset(num_triangle=5000, points_per_triangle=1000, smooth_factor=20, filename=dataset_path)
+    # dataset_path = f'{root_path}/triangle_sdf_dataset_smf20_arc_ratio_5000.csv'
+    # # dataset_path = '../../mnt/local/data/kalexu97/topOpt/rounded_triangle_sdf_dataset_smf20_arc_ratio.csv'
+    # df = generate_rounded_triangle_sdf_dataset(num_triangle=5000, points_per_triangle=1000, smooth_factor=20, filename=dataset_path)
 
-    dataset_path = f'{root_path}/quadrangle_sdf_dataset_smf20_arc_ratio_5000.csv'
-    # dataset_path = '../../mnt/local/data/kalexu97/topOpt/rounded_quadrangle_sdf_dataset_smf20_arc_ratio.csv'
-    df = generate_rounded_quadrangle_sdf_dataset(num_quadrangle=5000, points_per_quadrangle=1000, smooth_factor=20, filename=dataset_path)
+    # dataset_path = f'{root_path}/quadrangle_sdf_dataset_smf20_arc_ratio_5000.csv'
+    # # dataset_path = '../../mnt/local/data/kalexu97/topOpt/rounded_quadrangle_sdf_dataset_smf20_arc_ratio.csv'
+    # df = generate_rounded_quadrangle_sdf_dataset(num_quadrangle=5000, points_per_quadrangle=1000, smooth_factor=20, filename=dataset_path)
 
-    # #################### surface dataset ####################
+    # # #################### surface dataset ####################
 
-    dataset_path = f'{root_path}/ellipse_sdf_surface_dataset_smf22_150.csv'
-    # dataset_path = '../../mnt/local/data/kalexu97/topOpt/ellipse_sdf_surface_dataset_smf22' # without .csv!!
-    df, points_df = generate_ellipse_sdf_surface_dataset(num_ellipse=150, points_per_ellipse=1225, smooth_factor=22, filename=dataset_path)
+    # dataset_path = f'{root_path}/ellipse_sdf_surface_dataset_smf22_150.csv'
+    # # dataset_path = '../../mnt/local/data/kalexu97/topOpt/ellipse_sdf_surface_dataset_smf22' # without .csv!!
+    # df, points_df = generate_ellipse_sdf_surface_dataset(num_ellipse=150, points_per_ellipse=1225, smooth_factor=22, filename=dataset_path)
 
-    dataset_path = f'{root_path}/quadrangle_sdf_surface_dataset_smf20_150.csv'
-    # dataset_path = '../../mnt/local/data/kalexu97/topOpt/rounded_quadrangle_sdf_surface_dataset_smf20' # without .csv!!
-    df, points_df = generate_rounded_quadrangle_sdf_surface_dataset(num_quadrangle=150, points_per_quadrangle=1225, smooth_factor=20, filename=dataset_path)
+    # dataset_path = f'{root_path}/quadrangle_sdf_surface_dataset_smf20_150.csv'
+    # # dataset_path = '../../mnt/local/data/kalexu97/topOpt/rounded_quadrangle_sdf_surface_dataset_smf20' # without .csv!!
+    # df, points_df = generate_rounded_quadrangle_sdf_surface_dataset(num_quadrangle=150, points_per_quadrangle=1225, smooth_factor=20, filename=dataset_path)
 
-    dataset_path = f'{root_path}/triangle_sdf_surface_dataset_smf20_150.csv'
-    # dataset_path = '../../mnt/local/data/kalexu97/topOpt/rounded_triangle_sdf_surface_dataset_smf20' # without .csv !!
-    df, points_df = generate_rounded_triangle_sdf_surface_dataset(num_triangle=150, points_per_triangle=1225, smooth_factor=20, filename=dataset_path)
+    # dataset_path = f'{root_path}/triangle_sdf_surface_dataset_smf20_150.csv'
+    # # dataset_path = '../../mnt/local/data/kalexu97/topOpt/rounded_triangle_sdf_surface_dataset_smf20' # without .csv !!
+    # df, points_df = generate_rounded_triangle_sdf_surface_dataset(num_triangle=150, points_per_triangle=1225, smooth_factor=20, filename=dataset_path)
 
-    #################### test dataset ####################
+    # #################### test dataset ####################
 
-    dataset_path = f'{root_path}/ellipse_sdf_dataset_smf22_arc_ratio_500_test.csv'
-    # dataset_path = '../../mnt/local/data/kalexu97/topOpt/ellipse_sdf_dataset_smf22_arc_ratio.csv'
-    df = generate_ellipse_sdf_dataset(num_ellipse=500, points_per_ellipse=1000, smooth_factor=22, filename=dataset_path)
+    # dataset_path = f'{root_path}/ellipse_sdf_dataset_smf22_arc_ratio_500_test.csv'
+    # # dataset_path = '../../mnt/local/data/kalexu97/topOpt/ellipse_sdf_dataset_smf22_arc_ratio.csv'
+    # df = generate_ellipse_sdf_dataset(num_ellipse=500, points_per_ellipse=1000, smooth_factor=22, filename=dataset_path)
 
-    dataset_path = f'{root_path}/triangle_sdf_dataset_smf20_arc_ratio_500_test.csv'
-    # dataset_path = '../../mnt/local/data/kalexu97/topOpt/rounded_triangle_sdf_dataset_smf20_arc_ratio.csv'
-    df = generate_rounded_triangle_sdf_dataset(num_triangle=500, points_per_triangle=1000, smooth_factor=20, filename=dataset_path)
+    # dataset_path = f'{root_path}/triangle_sdf_dataset_smf20_arc_ratio_500_test.csv'
+    # # dataset_path = '../../mnt/local/data/kalexu97/topOpt/rounded_triangle_sdf_dataset_smf20_arc_ratio.csv'
+    # df = generate_rounded_triangle_sdf_dataset(num_triangle=500, points_per_triangle=1000, smooth_factor=20, filename=dataset_path)
 
-    dataset_path = f'{root_path}/quadrangle_sdf_dataset_smf20_arc_ratio_500_test.csv'
-    # dataset_path = '../../mnt/local/data/kalexu97/topOpt/rounded_quadrangle_sdf_dataset_smf20_arc_ratio.csv'
-    df = generate_rounded_quadrangle_sdf_dataset(num_quadrangle=500, points_per_quadrangle=1000, smooth_factor=20, filename=dataset_path)
+    # dataset_path = f'{root_path}/quadrangle_sdf_dataset_smf20_arc_ratio_500_test.csv'
+    # # dataset_path = '../../mnt/local/data/kalexu97/topOpt/rounded_quadrangle_sdf_dataset_smf20_arc_ratio.csv'
+    # df = generate_rounded_quadrangle_sdf_dataset(num_quadrangle=500, points_per_quadrangle=1000, smooth_factor=20, filename=dataset_path)
 
-    #################### radius dataset ####################
+    # #################### radius dataset ####################
 
-    dataset_path = f'{root_path}/triangle_sdf_dataset_smf40_radius_sample_100'
-    df = generate_traingle_random_radius_dataset(num_triangle=100, sample_per_triangle=100, smooth_factor=40, filename=dataset_path)
+    # dataset_path = f'{root_path}/triangle_sdf_dataset_smf40_radius_sample_100'
+    # df = generate_traingle_random_radius_dataset(num_triangle=100, sample_per_triangle=100, smooth_factor=40, filename=dataset_path)
 
-    dataset_path = f'{root_path}/quadrangle_sdf_dataset_smf40_radius_sample_100'
-    df = generate_quadrangle_random_radius_dataset(num_quadrangle=100, sample_per_quadrangle=100, smooth_factor=40, filename=dataset_path)
+    # dataset_path = f'{root_path}/quadrangle_sdf_dataset_smf40_radius_sample_100'
+    # df = generate_quadrangle_random_radius_dataset(num_quadrangle=100, sample_per_quadrangle=100, smooth_factor=40, filename=dataset_path)
 
     #################### reconstruction dataset ####################
 
-    n_features_per_shape = 600000
+    n_features_per_shape = 5000000
     # root_path = '../shape_datasets'
 
     # training dataset
--- train/AE_reconstructor_training.py
+++ train/AE_reconstructor_training.py
@@ -71,7 +71,7 @@ def main(args):
     config_name = args.config_name
     models_dir = args.model_dir
     saved_model_path = f'{models_dir}/uba_{config_name}.pt'
-    run_name = f'uba_{config_name}'
+    run_name = f'uba_{config_name}_recon_dec'
 
     dataset_train_files = [f'{dataset_path}/ellipse_reconstruction_dataset_train.csv',
                        f'{dataset_path}/triangle_reconstruction_dataset_train.csv',
@@ -164,12 +164,12 @@ def main(args):
 
 
     # Save model weights
-    checkpoint_path = f'{models_dir}/{run_name}_full.ckpt'
+    checkpoint_path = f'{models_dir}/{config_name}_full.ckpt'
     trainer.save_checkpoint(checkpoint_path)
     print(f"Model weights saved to {checkpoint_path}")
 
     # Save just the model weights
-    model_weights_path = f'{models_dir}/{run_name}_full.pt'
+    model_weights_path = f'{models_dir}/{config_name}_full.pt'
     torch.save(vae_model.state_dict(), model_weights_path)
     print(f"Model weights saved to {model_weights_path}")
 
