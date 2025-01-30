b5400fbe16d55cdbbfa0bd06249f305b9eb60493
Alexander19970212 <kalexu97@mail.ru>
2025-01-30 17:15:00 +0300

    [tch]

--- scripts/dock2_VAEs_frst_strtg.sh
+++ scripts/dock2_VAEs_frst_strtg.sh
@@ -7,8 +7,8 @@ CONFIG_DIR="configs/NN_sdf_experiments/final_experiments/VAEs"
 MAX_EPOCHS=1  # Adjust as needed
 DATASET_PATH="shape_datasets"
 
-METRICS_FILE="src/final_metrics_round2_frst_strtg_VAEs.json"
-METRICS_FILE_RECON="src/final_metrics_round2_frst_strtg_VAEs_recon.json"
+METRICS_FILE="src/final_metrics_round1_frst_strtg_VAEs.json"
+METRICS_FILE_RECON="src/final_metrics_round1_frst_strtg_VAEs_recon.json"
 
 
 for CONFIG_FILE in "$CONFIG_DIR"/*.yaml; do
