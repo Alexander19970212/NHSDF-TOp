43320a19f5b774709409fbc482b4f12479df3329
Alexander19970212 <kalexu97@mail.ru>
2025-03-20 16:16:59 +0300

    [tch]

--- scripts/plot_latent_distribution.py
+++ scripts/plot_latent_distribution.py
@@ -122,8 +122,8 @@ def main(args):
         "axes_positions": [
             [-140, 50],
             [-120, -120],
+            [85, -125],
             [85, -95],
-            [40, -125],
             [80, 100]
         ],
         "gf_types":[ # n: doesn't matter, c: circle, t: triangle, q: quadrangle
