12c6fc697553e8f433f61f015705551461b19ad2
Alexander19970212 <kalexu97@mail.ru>
2025-03-20 14:34:29 +0300

    [tch]

--- vizualization_utils.py
+++ vizualization_utils.py
@@ -406,7 +406,7 @@ def plot_latent_space(model, dataloader, num_samples=4000, filename = None):
         for tsne_idx, tsne_coord in enumerate(tsne_coords):
             # ax_inset = fig.add_axes([tsne_coord[0], tsne_coord[1], 0.18, 0.18])
             print(tsne_coord)
-            ax_inset = fig.add_axes(0, 0, 0.18, 0.18)
+            ax_inset = fig.add_axes([0, 0, 0.18, 0.18])
             chi_pred = chis[tsne_idx]
             geometry_type, geometry_params = extract_geometry(chi_pred)
             draw_geometry(geometry_type, geometry_params, ax_inset)
