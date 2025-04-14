b6c8e1da90d9b724ce54b9aeab01ab7da6969577
Alexander19970212 <kalexu97@mail.ru>
2025-04-14 12:39:03 +0300

    [tch]

--- datasets/SDF_dataset.py
+++ datasets/SDF_dataset.py
@@ -213,7 +213,7 @@ class ReconstructionDataset(Dataset):
                     df["class"] = class_i/(self.n_classes-1)
 
         self.data = pd.concat(dfs, ignore_index=True)
-        print(self.data.columns)
+        # print(self.data.columns)
         valid_feature_names = [col for col in feature_names if col in self.data.columns]
         self.data = self.data.reindex(columns=valid_feature_names, fill_value=0)
         self.data = self.data.fillna(0)
@@ -226,8 +226,8 @@ class ReconstructionDataset(Dataset):
     def __getitem__(self, idx):
         row = self.data.iloc[idx]
         X_row = row.drop(labels=['arc_ratio'])
-
-        X = torch.tensor(X_row.values, dtype=torch.float32)
+        X = torch.zeros(self.feature_dim+2, dtype=torch.float32)
+        X[2:] = torch.tensor(X_row.values, dtype=torch.float32)
         arc_ratio = torch.tensor(row['arc_ratio'].item(), dtype=torch.float32)
         
         return X, torch.tensor(0, dtype=torch.float32), arc_ratio
