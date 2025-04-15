f58fe459d2f4c1bd2706b1a5f5101eadb293adbf
Alexander19970212 <kalexu97@mail.ru>
2025-04-15 12:21:54 +0300

    [tch]

--- scripts/VAEs_frst_strtg_quad.sh
+++ scripts/VAEs_frst_strtg_quad.sh
@@ -16,23 +16,23 @@ METRICS_FILE_RECON="src/final_metrics_frst_strtg_VAEs_quad_recon.json"
 
 # for CONFIG_FILE in "$CONFIG_DIR"/*.yaml; do
 CONFIG_NAME="VAE_DeepSDF_quad"
-RUN_NAME="quad"
+RUN_NAME="Bprec"
 DATASET_TYPE="quadrangle"
 
-# echo "Running VAE_training.py with config: $CONFIG_NAME"
+echo "Running VAE_training.py with config: $CONFIG_NAME"
 
-# python train/global_training_via_HvDecoder.py \
-#     --max_epochs "$MAX_EPOCHS" \
-#     --dataset_path "$DATASET_PATH" \
-#     --config_dir "$CONFIG_DIR" \
-#     --config_name "$CONFIG_NAME" \
-#     --metrics_file "$METRICS_FILE" \
-#     --run_name "$RUN_NAME" \
-#     --dataset_type "$DATASET_TYPE"
+python train/global_training_via_HvDecoder.py \
+    --max_epochs "$MAX_EPOCHS" \
+    --dataset_path "$DATASET_PATH" \
+    --config_dir "$CONFIG_DIR" \
+    --config_name "$CONFIG_NAME" \
+    --metrics_file "$METRICS_FILE" \
+    --run_name "$RUN_NAME" \
+    --dataset_type "$DATASET_TYPE"
 
-# echo "Completed run: $RUN_NAME"
-# echo "----------------------------------------"
-# done
+echo "Completed run: $RUN_NAME"
+echo "----------------------------------------"
+done
 
 # for CONFIG_FILE in "$CONFIG_DIR"/*.yaml; do
 # CONFIG_NAME=$(basename "$CONFIG_FILE" .yaml)
